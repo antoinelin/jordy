18d2575baf7adf44194ca280e250ec64a2f938c7
toinelin <contact@antoinelin.com>
2018-11-14 01:38:59 +0100

    Doc: change wordpress installation title

--- docker/wordpress/install-wp.sh
+++ docker/wordpress/install-wp.sh
@@ -2,7 +2,7 @@
 
 # Install WordPress.
 wp core install \
-  --title="WordPress Gatsby" \
+  --title="WordPress" \
   --admin_user="$WORDPRESS_ADMIN_USER" \
   --admin_password="$WORDPRESS_ADMIN_PASSWORD" \
   --admin_email="$WORDPRESS_ADMIN_EMAIL" \
