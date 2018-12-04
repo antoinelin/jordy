0763064c8ebbe1b750c3164ac3586da82ad3031b
toinelin <contact@antoinelin.com>
2018-12-04 01:49:25 +0100

    Feat: add wp api JWT and setup redis cache

--- docker/wordpress/install-wp.sh
+++ docker/wordpress/install-wp.sh
@@ -11,7 +11,7 @@ wp core install \
   --url="https://jordy.dev"
 
 # Update permalink structure.
-wp option update permalink_structure "/%postname%" --skip-themes --skip-plugins
+wp option update permalink_structure "/%postname%/" --skip-themes --skip-plugins
 
 # Install needed plugins
 wp plugin install --activate \
@@ -19,7 +19,14 @@ wp plugin install --activate \
   redis-cache \
   advanced-custom-fields \
   acf-to-rest-api \
-  wp-rest-api-log
+  wp-rest-api-log \
+  jwt-authentication-for-wp-rest-api
 
 # Config Redis on wp-config.php
 wp config set WP_REDIS_HOST 'redis' --type=constant
+wp config set WP_CACHE_KEY_SALT $WP_CACHE_KEY_SALT --type=constant
+wp config set CACHE true --type=constant --raw
+
+# Config WordPress API JWT auth plugin
+wp config set JWT_AUTH_SECRET_KEY $JWT_AUTH_SECRET_KEY --type=constant
+wp config set JWT_AUTH_CORS_ENABLE true --type=constant --raw
