1264b0db45ba883760a1db180f7507fc4604afdb
toinelin <contact@antoinelin.com>
2018-12-08 22:02:59 +0100

    Feat: add screenshot, primary content and version 1

--- wp-content/themes/jordy-theme/index.php
+++ wp-content/themes/jordy-theme/index.php
@@ -4,10 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
     <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
-
+    <main class='jordy-main'>
+        <h1>Jordy WordPress API Theme</h1>
+        <p>If you want WordPress to redirect to your front-end website, please change WordPress URL in your dashboard panel (Settings > General).</br></br>If you encounter any problems or you have any ideas to improve this project, please open an issue on <a href="https://github.com/toinelin/jordy/issues" target="_blank"rel="noreferrer noopener">GitHub<a>.</p>
+    </main>
 <?php wp_footer(); ?>
 </body>
 </html>
